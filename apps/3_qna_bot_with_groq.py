b4c2a2e3b4ca74846f126a8882b70d75f1599aed
Aakshat-01 <aakshatgupta40@gmail.com>
2026-09-06 21:11:30 +0530

    Made an AI Agent who can answer at a high speed, do google searches, streams the message, and has memory

---
+++ apps/3_qna_bot_with_groq.py
@@ -0,0 +1,63 @@
+## LLM
+## Tool - Google Search
+## Agent
+## Memory
+## Streaming
+## Web Interface - Streamlit 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_groq import ChatGroq
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
+import streamlit as st
+
+llm = ChatGroq(model="openai/gpt-oss-20b", streaming = True)
+search = GoogleSerperAPIWrapper()
+tools = [search.run] 
+
+if "memory" not in st.session_state:
+    st.session_state.memory= MemorySaver()
+    st.session_state.history=[]
+
+agent = create_agent(
+    model=llm,
+    tools= tools,
+    checkpointer=st.session_state.memory,
+    system_prompt="You are a Amazing AI Agent and can search on google as well."
+)
+
+
+## Building Web Interface
+
+st.subheader("QuickAnswer - Your Fastest QnA Bot")
+
+for message in st.session_state.history:
+    role = message["role"]
+    content = message["content"]
+    st.chat_message(role).markdown(content)
+
+
+query = st.chat_input("Ask Anything")
+if query:
+    st.chat_message("user").markdown(query)
+    st.session_state.history.append({"role":"user", "content":query})
+
+    res = agent.stream(
+        {"messages":[{"role":"user", "content":query}]},
+        {"configurable": {"thread_id": "1"}},
+        stream_mode="messages"
+    )
+
+    ai_container= st.chat_message("ai")
+    with ai_container:
+        space = st.empty()
+
+        message=""
+        for chunk in res: 
+            message = message + chunk[0].content  #type:ignore
+            space.write(message)
+
+        st.session_state.history.append({"role":"ai", "content":message})
